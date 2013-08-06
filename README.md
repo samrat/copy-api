af6ef0943b08b41a9c6c7c477fe8782f68bcf878
Samrat Man Singh <samratmansingh@gmail.com>
2013-08-06 21:46:52 +0545

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# copy-api
+
+A Clojure library to work with the Copy.com API.
+
+**Note: This is not ready yet. Don't use this.**
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2013 Samrat Man Singh
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject copy-api "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-oauth "1.4.1-SNAPSHOT"]
+                 [clj-http "0.7.6"]
+                 [cheshire "5.2.0"]])

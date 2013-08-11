3ce13948c82334645fbe5924a7b18df7ce9e9225
Samrat Man Singh <samratmansingh@gmail.com>
2013-08-11 14:17:05 +0545

    Inc version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject copy-api "0.1.3-SNAPSHOT"
+(defproject copy-api "0.1.4-SNAPSHOT"
   :description "A Clojure wrapper for the Copy.com API"
   :url "https://github.com/samrat/copy-api"
   :license {:name "Eclipse Public License"
